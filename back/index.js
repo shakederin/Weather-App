58147a04a1a0df2caa766797b0db738920b13fa6
shakederin <89574610+shakederin@users.noreply.github.com>
2022-01-02 12:13:41 +0200

    setUpForDEPLOY

---
+++ back/index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const cors = require("cors")
+const path = require("path")
+const app = express()
+
+app.use(cors())
+app.use(express.static(path.join(__dirname, '../front/build/')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../front/build/index.html'));
+});
+
+app.listen(process.env.PORT || 3006, ()=>{
+    console.log("running");
+})
